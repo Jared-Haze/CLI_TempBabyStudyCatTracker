4e9e6da9e4a6f1e73ee328b13f8882df56d4eb8e
Jared Osei <jaredthomasmitchealoseirosa@gmail.com>
2026-07-23 02:10:40 -0400

    Added logic for displaying title (of user & VegetaBot). Final update for a while (potentially 4ever)

--- app/src/main/java/org/example/userNPCs.java
+++ app/src/main/java/org/example/userNPCs.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class userNPCs {
     int id;
     String name;
@@ -12,7 +14,21 @@ public class userNPCs {
     }
 
     public String getUNPC(){
-        String userStats = name + " | powerLVL : " + pwrLvl + " | title : ";
+        String title;
+        if(pwrLvl >= 450){
+            title = "supreme learning being (make the next study program)";
+        }else if(pwrLvl >= 375){
+            title = "study God";
+        }else if(pwrLvl >= 300){
+            title = "study master";
+        }else if(pwrLvl >= 225){
+            title = "study warrior";
+        }else if(pwrLvl >= 150){
+            title = "study trainee";
+        }else{
+            title = "study baby";
+        }
+        String userStats = name + " | powerLVL : " + pwrLvl + " | title : " + title;
         return userStats;
     }
 }
